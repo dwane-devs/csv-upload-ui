2ae745ad20856c8e3e75e04905d97f78fb470365
Itchy Panda <Itchy@example.com>
2022-05-01 12:43:30 -0400

    completed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-
+import { useState } from 'react';
+import { parse } from "papaparse"
 import './App.css';
 
 function App() {
+
+  const [highlighted, setHighlighted] = useState(false)
+  const [contacts, setContacts] = useState([{email: "fake@gmail.com", name: "Fake"}])
+
   return (
     <div className="App">
       <h1>Contact Import</h1>
-      <div className="drop-zone"> DROP HERE</div>
-  
+      <div className={`drop-zone ${highlighted && "highlight"} `}
+      onDragEnter={(e)=>{
+        setHighlighted(true)
 
+      }}
+      onDragLeave={(e)=>{
+        setHighlighted(false)
+        
+      }}
+      onDragOver={(e)=>{
+        e.preventDefault()
+      }}
+      onDrop={(e)=>{
+        e.preventDefault()
+        setHighlighted(false)
+        console.log(e.dataTransfer.files)
+        Array.from(e.dataTransfer.files)
+        .filter((file) => file.type === "text/csv" )
+        .forEach( async (file) => { 
+          // console.log(file)
+          const text = await file.text();
+          // console.log(text)
+          const result = parse(text, {header: true} )
+          console.log(result.data)
+          setContacts(existing => [...existing, ...result.data])
+        })
+      }}
+      > DROP HERE</div>
+  
+  <ul>
+    {contacts.map(((contact)=>(
+      <li key={contact.email}>{contact.name}</li>
+    )))}
+      
+    </ul>
     </div>
+  
   );
 }
 
